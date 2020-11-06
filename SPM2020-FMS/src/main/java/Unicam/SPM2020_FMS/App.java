96b809c3c069ab416df080be2a3d81125258b9de
mario <mario.lombardi@studenti.unicam.it>
2020-11-06 16:17:08 +0100

    Commit for the setting of a Maven Project.

---
+++ SPM2020-FMS/src/main/java/Unicam/SPM2020_FMS/App.java
@@ -0,0 +1,13 @@
+package Unicam.SPM2020_FMS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
