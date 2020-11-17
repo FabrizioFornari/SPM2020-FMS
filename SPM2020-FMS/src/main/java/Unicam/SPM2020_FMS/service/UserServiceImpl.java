f11cb5e6c357903a50957471e4e5cb6e41b2bade
teoful <tful@hotmail.it>
2020-11-17 19:40:40 +0100

    Added Password Encryption on UserService

--- SPM2020-FMS/src/main/java/Unicam/SPM2020_FMS/service/UserServiceImpl.java
+++ SPM2020-FMS/src/main/java/Unicam/SPM2020_FMS/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package Unicam.SPM2020_FMS.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import Unicam.SPM2020_FMS.model.Login;
 import Unicam.SPM2020_FMS.model.User;
@@ -8,14 +10,18 @@ import Unicam.SPM2020_FMS.dao.UserDao;
 
 public class UserServiceImpl implements UserService {
 
+  private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
   @Autowired
   public UserDao userDao;
 
   public int register(User user) {
+	user.setPassword(passwordEncoder.encode(user.getPassword()));
     return userDao.register(user);
   }
 
   public User validateUser(Login login) {
+	login.setPassword(passwordEncoder.encode(login.getPassword()));
     return userDao.validateUser(login);
   }
 
