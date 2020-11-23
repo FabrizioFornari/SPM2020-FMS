81aedb2e8446a05059987499d2c5aaf918d01f41
Stefano Aschettino <stefano.aschettino@studenti.unicam.it>
2020-11-23 10:30:06 +0100

    Added Selenium test for the Home Page on Chrome

---
+++ SPM2020-FMS/src/test/java/Unicam/SPM2020_FMS/SeleniumHomePageChrome.java
@@ -0,0 +1,50 @@
+package Unicam.SPM2020_FMS;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+class SeleniumHomePageChrome {
+	
+	static String projectPath;
+	static WebDriver driver;
+	static String URLbase;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		projectPath = System.getProperty("user.dir");
+		URLbase = "http://localhost:8080/SPM2020-FMS/";
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/Linux/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--no-sandbox");
+		driver = new ChromeDriver(options);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
+	}
+
+	@Test
+	void checkHomePage() throws InterruptedException {
+		driver.get(URLbase+"home.jsp");
+		Thread.sleep(3000);
+	}
+}
